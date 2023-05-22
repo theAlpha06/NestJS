27bee6658c696ef3c7b59876a350cf9029c10172
Shubham <95537249+theAlpha06@users.noreply.github.com>
2023-05-22 22:52:31 +0530

    Model of task
    I have defined the model of task using interface

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { Task } from './task.model';
 
 @Injectable() //Injectible makes it possible to inject this service into other components(makes it singleton)
 export class TasksService {
-  private tasks = []; //This is a temporary database
+  private tasks: Task[] = []; //This is a temporary database
 
-  getAllTasks() {
+  getAllTasks(): Task[] {
     return this.tasks;
   }
 }
