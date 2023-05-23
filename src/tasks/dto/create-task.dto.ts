d4ef057db4b7da7ff99cc81a301be0c0b43ecf18
Shubham <95537249+theAlpha06@users.noreply.github.com>
2023-05-23 22:35:51 +0530

    add validation using pipes

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,6 +1,10 @@
 /* eslint-disable prettier/prettier */
+
+import { IsNotEmpty } from 'class-validator';
 export class CreateTaskDto { 
   // Data Transfer Object
+  @IsNotEmpty()
   title: string;
+  @IsNotEmpty()
   description: string;
 }
