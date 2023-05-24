a3faf5f0ad40703083a8d451587bd151368afbaf
Shubham <95537249+theAlpha06@users.noreply.github.com>
2023-05-24 23:01:39 +0530

    get all task

--- src/tasks/task.repository.ts
+++ src/tasks/task.repository.ts
@@ -3,9 +3,17 @@ import { EntityRepository, Repository } from "typeorm";
 import { Task } from "./task.entity";
 import { CreateTaskDto } from "./dto/create-task.dto";
 import { TaskStatus } from "./task-status.enum";
+import { GetTasksFilterDto } from "./dto/get-tasks-filter.dto";
 
 @EntityRepository(Task)
 export class TasksRepository extends Repository<Task> {
+
+  async getTasks(filterDto: GetTasksFilterDto): Promise<Task[]> {
+    const query = this.createQueryBuilder('task');
+    
+    const tasks = await query.getMany();
+    return tasks;
+  }
   async createTask(createTaskDto: CreateTaskDto): Promise<Task> {
     const { title, description } = createTaskDto;
     const task = this.create({
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -5,13 +5,16 @@ import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import { TasksRepository } from './task.repository';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Task } from './task.entity';
-import { Not } from 'typeorm';
 
 @Injectable() //Injectible makes it possible to inject this service into other components(makes it singleton)
 export class TasksService {
   constructor(
     @InjectRepository(TasksRepository) private tasksRepository: TasksRepository,
   ) {}
+
+  getTasks(filterDto: GetTasksFilterDto): Promise<Task[]> {
+    return this.tasksRepository.getTasks(filterDto);
+  }
 
   async getTaskById(id: string): Promise<Task> {
     const found = await this.tasksRepository.findOne(id);
@@ -20,31 +23,6 @@ export class TasksService {
     }
     return found;
   }
-  // private tasks: Task[] = []; This is a temporary database
-  // getAllTasks(): Task[] {
-  //   return this.tasks;
-  // }
-  // getTasksWithFilters(filterDto: GetTasksFilterDto): Task[] {
-  //   const { status, search } = filterDto;
-  //   //define a temp array to hold the result
-  //   let tasks = this.getAllTasks();
-  //   //do something with status
-  //   if (status) {
-  //     tasks = tasks.filter((task) => task.status === status);
-  //   }
-  //   //do something with search
-  //   if (search) {
-  //     tasks = tasks.filter((task) => {
-  //       if (task.title.includes(search) || task.description.includes(search)) {
-  //         return true;
-  //       } else {
-  //         return false;
-  //       }
-  //     });
-  //   }
-  //   //return result
-  //   return tasks;
-  // }
   createTask(createTaskDto: CreateTaskDto): Promise<Task> {
     return this.tasksRepository.createTask(createTaskDto);
   }
