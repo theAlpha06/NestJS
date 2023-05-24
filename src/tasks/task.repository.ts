b36b582c221606971f84b165f40b8c747d6a2e12
Shubham <95537249+theAlpha06@users.noreply.github.com>
2023-05-24 21:40:05 +0530

    create task repository

---
+++ src/tasks/task.repository.ts
@@ -0,0 +1,8 @@
+/* eslint-disable prettier/prettier */
+import { EntityRepository, Repository } from "typeorm";
+import { Task } from "./task.entity";
+
+@EntityRepository(Task)
+export class TasksRepository extends Repository<Task> {
+
+}
