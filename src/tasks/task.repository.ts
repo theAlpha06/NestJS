f0bdd5e0c6f5a9d83d47cdda5b8e08bad2011485
Shubham <95537249+theAlpha06@users.noreply.github.com>
2023-05-24 23:08:10 +0530

    search and status filter

--- src/tasks/task.repository.ts
+++ src/tasks/task.repository.ts
@@ -9,7 +9,19 @@ import { GetTasksFilterDto } from "./dto/get-tasks-filter.dto";
 export class TasksRepository extends Repository<Task> {
 
   async getTasks(filterDto: GetTasksFilterDto): Promise<Task[]> {
+    const { status, search } = filterDto;
     const query = this.createQueryBuilder('task');
+
+    if (status) {
+      query.andWhere('task.status = :status', { status });
+    }
+
+    if (search) {
+      query.andWhere(
+        'task.title LIKE :search OR task.description LIKE :search',
+        { search: `%${search}%`}
+      )
+    }
     
     const tasks = await query.getMany();
     return tasks;
