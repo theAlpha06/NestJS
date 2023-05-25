3e7837b9305a32ebd2fdd10dd3fe68ebe834439b
Shubham <95537249+theAlpha06@users.noreply.github.com>
2023-05-25 15:34:12 +0530

    Authentication
    Create entity of user and auth module

---
+++ src/auth/user.entity.ts
@@ -0,0 +1,15 @@
+/* eslint-disable prettier/prettier */
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity() 
+export class User {
+  
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  username: string;
+
+  @Column()
+  password: string;
+}
---
+++ src/auth/users.repository.ts
@@ -0,0 +1,8 @@
+/* eslint-disable prettier/prettier */
+import { EntityRepository, Repository } from "typeorm";
+import { User } from "./user.entity";
+
+@EntityRepository(User)
+export class UsersRepository extends Repository<User> {
+  
+}
